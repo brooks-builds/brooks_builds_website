f7287a65feaa913d0e96c5516f314aa240dd870d
Brooks Patton <brooks@brookzerker.com>
2020-05-24 15:24:16 -0600

    Links to where to find me

--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -6,6 +6,26 @@
         <v-row>
             <p>Every weekday <span class="bold">7am - 8:15am</span> Mountain Time</p>
         </v-row>
+        <v-row>
+            <v-col>
+                <v-btn block color="#9147ff" dark href="https://www.twitch.tv/brookzerker" target="window">Follow on Twitch</v-btn>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col>
+                <v-btn block color="#ff0000" dark href="https://www.youtube.com/channel/UCT1-XRVnJA-wws2bfbLbFcQ" target="window">Subscribe on YouTube</v-btn>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col>
+                <v-btn block color="#1da1f2" dark href="https://www.twitter.com/brooks_patton" target="window">Follow on Twitter</v-btn>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col>
+                <v-btn block color="#24292e" dark href="https://www.github.com/brooks-builds" target="window">See the Code on GitHub</v-btn>
+            </v-col>
+        </v-row>
     </v-container>
 </template>
 
