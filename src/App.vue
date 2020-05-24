35bce2b1483970f84ea0671f429dee28cca3100f
Brooks Patton <brooks@brookzerker.com>
2020-05-24 14:22:24 -0600

    About Brooks Builds showing

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,28 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark class="d-flex justify-space-around mb-6">
-      <v-btn
-        text
-      >
-        <span class="mr-2">Home</span>
-      </v-btn>
-      <v-btn text class="mr-2">
-        <span class="mr-2">Recent Videos</span>
-      </v-btn>
-      <v-btn text class="mr-2">
-        <span class="mr-2">Chat Commands</span>
-      </v-btn>
-      <v-btn text class="mr-2">
-        <span class="mr-2">Current Projects</span>
-      </v-btn>
-      <v-btn text class="mr-2">
-        <span class="mr-2">Project Suggestions</span>
-      </v-btn>
-    </v-app-bar>
+    <Navigation />
 
     <v-content>
+      <v-container>
+        <v-row>
+          <v-col cols="3">
+          <About />
+          </v-col>
+        </v-row>
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
+import Navigation from './components/Navigation';
+import About from './components/About'
 export default {
   name: "App",
 
   components: {
+    Navigation,
+    About,
   },
 
   data: () => ({
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,30 +1,32 @@
 <template>
-  <h1>hello</h1>
+  <v-app-bar app color="primary" dark class="d-flex justify-space-around mb-6">
+      <v-btn
+        text
+      >
+        <span class="mr-2">Home</span>
+      </v-btn>
+      <v-btn text class="mr-2">
+        <span class="mr-2">Recent Videos</span>
+      </v-btn>
+      <v-btn text class="mr-2">
+        <span class="mr-2">Chat Commands</span>
+      </v-btn>
+      <v-btn text class="mr-2">
+        <span class="mr-2">Current Projects</span>
+      </v-btn>
+      <v-btn text class="mr-2">
+        <span class="mr-2">Project Suggestions</span>
+      </v-btn>
+    </v-app-bar>
 </template>
 
 <script>
 export default {
-  name: "Navigation",
-  props: {
-    msg: String
-  }
+  name: "Navigation"
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
